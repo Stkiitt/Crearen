9cad8399b4c431827040b9af9097e7e248a3438e
Masuda <k021c1193@g.neec.ac.jp>
2022-11-17 11:16:34 +0900

    利用規約記入、CSS記入箇所変更

--- pages/taskadmin.vue
+++ pages/taskadmin.vue
@@ -5,7 +5,7 @@
         aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <NuxtLink class="navbar-brand" to="/"> <img style="width: 9em;" src="~/assets/logo/logo.png"></NuxtLink>
+      <NuxtLink class="navbar-brand" to="/"> <img id="logo" src="~/assets/logo/logo.png"></NuxtLink>
       <div class="collapse navbar-collapse justify-content-end">
         <ul class="navbar-nav">
           <li class="nav-item">
@@ -99,15 +99,6 @@
       </div>
     </div>
 
-        <div class="col-4">
-          <div class="m-2 p-3 border">
-            <h2 class="my-4">遊び要素</h2>
-          </div>
-
-        </div>
-      </div>
-    </div>
-    
     <!-- Footer -->
     <footer class="text-center text-lg-start bg-light text-muted mt-4">
       <!-- Section: Links  -->
@@ -149,7 +140,7 @@
       <!-- Section: Links  -->
 
       <!-- Copyright -->
-      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      <div class="text-center p-4" id="copyright">
         © 2022 Copyright:
         <a class="text-reset fw-bold" href="#">卒業制作を頑張る会</a>
       </div>
@@ -284,6 +275,14 @@ export default {
 </script>
 
 <style>
+#logo{
+  width: 9em;
+}
+
+#copyright{
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
 #ToDo {
   display: inline;
 }
--- pages/terms.vue
+++ pages/terms.vue
@@ -5,10 +5,28 @@
         aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <NuxtLink class="navbar-brand" to="/"> <img style="width: 9em;" src="~/assets/logo/logo.png"></NuxtLink>
+      <NuxtLink class="navbar-brand" to="/"> <img id="logo" src="~/assets/logo/logo.png"></NuxtLink>
     </nav>
 
-    まだなにもないよ
+    <div class="container mt-5 mb-5">
+        <h1>利用規約</h1>
+        <hr>
+        <p>この利用規約（以下，「本規約」といいます。）は、Crearen（以下、「当社」といいます。）がこのウェブサイト上で提供するサービス（以下、「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下、「ユーザー」といいます。）には、本規約に従って本サービスをご利用いただきます。</p>
+
+        <h2>第一条(適用)</h2>
+        <ol>
+            <li>本規約は、ユーザーと当社との～～～～～～～</li>
+            <li>当社は本サービスに関し、～～～～～～～～～</li>
+            <li>本規約の規定が前条の個別規定～～～～～～～</li>
+        </ol>
+
+        <h2 class="mt-5">第二条(プライバシーポリシー)</h2>
+        <ol>
+            <li>当社は、本サービスの利用によって取得する個人情報については～～～～～</li>
+            <li>君は誰</li>
+        </ol>
+
+    </div>
 
     <div class="text-center">
       <button type="button" class="btn btn-danger" onclick="window.close();">閉じる</button>
@@ -30,5 +48,8 @@ export default {
 </script>
 
 <style>
+#logo{
+  width: 9em;
+}
 
 </style>
