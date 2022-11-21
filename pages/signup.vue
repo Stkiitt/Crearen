b8790fa5827455fbe826a39f3a3ba01273a281c3
Stkiitt <ski279t@gmail.com>
2022-11-21 10:29:03 +0900

    新規登録と確認ポップアップ機能の作成

--- pages/signup.vue
+++ pages/signup.vue
@@ -41,7 +41,7 @@
               <h3 class="text-center">登録内容の確認</h3>
               <p><label for="username" class="ml-5 mt-5">ユーザー名　　　：{{ username }}</label></p>
               <p><label for="email" class="ml-5">メールアドレス　：{{ email }}</label></p>
-              <p><label for="exampleInputPassword1" class="ml-5 form-wrapper">パスワード　　　：{{ password }} </label></p>
+              <p><label for="exampleInputPassword1" class="ml-5 form-wrapper">パスワード　　　：{{ password }}</label></p>
               <p class="text-center mt-5"><button @click="signup()" id="confirmBtn"
                   class="btn btn-success col-4">登録する</button></p>
               <p class="text-center"><button @click="closeConfirmPopup()" class="btn btn-danger col-4"
