6e94757816da99a297e712d5643c4522f26690d6
Stkiitt <ski279t@gmail.com>
2022-12-20 14:52:42 +0900

    ソート機能の作成

--- pages/taskadmin.vue
+++ pages/taskadmin.vue
@@ -22,7 +22,7 @@
     <div class="container mt-5">
       <div style="display: flex;">
         <h2>タスク管理画面</h2>
-        <button @click="openHistoryPopup()" class="button-add">タスク履歴</button>
+        <button @click="openHistoryPopup()" class="button-history">タスク履歴</button>
         <!-- 履歴ポップアップここから -->
         <section id="taskHistory" class="modalArea">
           <div @click="closeHistoryPopup()" class="modalBg"></div>
@@ -55,7 +55,13 @@
         <div class="col-xl-8 order-xl-1 col-12 order-2">
           <div class="m-2 p-3 border">
             <h2 class="my-4" id="ToDo">ToDo</h2>
-            <button @click="openAddPopup()" class="button-add">＋追加</button>
+            <button @click="openAddPopup()" class="button-add">追加</button>
+            <select @change="sortTasks(Number($event.target.value))" class="sort_select">
+              <option value="0">新しい順</option>
+              <option value="1">古い順</option>
+              <option value="2">優先度順</option>
+              <option value="3">期限が近い順</option>
+            </select>
             <!-- 追加ポップアップここから -->
             <section id="addTask" class="modalArea">
               <div @click="closeAddPopup()" class="modalBg"></div>
@@ -312,6 +318,7 @@ export default {
         const task = doc.data();
         task["tid"] = doc.id;
         this.tasks.push(task);
+        this.sortTasks(0);
       });
     },
     // task_completedからデータの取得
@@ -325,6 +332,39 @@ export default {
         comptask["tid"] = doc.id;
         this.comptasks.push(comptask);
       });
+    },
+    // タスクソート機能（mode 0:新しい順、1:古い順、2:優先度が高い順、3:期限の近い順）
+    sortTasks(mode) {
+      switch (mode) {
+        case 0:
+          this.tasks.sort((a, b) => {
+            return b.time - a.time;
+          });
+          break;
+        case 1:
+          this.tasks.sort((a, b) => {
+            return a.time - b.time;
+          });
+          break;
+        case 2:
+          this.tasks.sort((a, b) => {
+            if (a.priority == b.priority) return 0;
+            else if (a.priority == "高") return -1;
+            else if (a.priority == "中") return -1;
+            return 0;
+          });
+          break;
+        case 3:
+          this.tasks.sort((a, b) => {
+            const ac = Number(a.deadline.replace(/-/g, ''));
+            const bc = Number(b.deadline.replace(/-/g, ''));
+            if (ac == bc) return 0;
+            else if (ac == 0) return 1;
+            else if (bc == 0) return -1;
+            else return ac - bc;
+          });
+          break;
+      }
     },
     //現在時刻の取得(秒単位まで)
     getNow() {
@@ -340,12 +380,12 @@ export default {
     },
     // 今日のYYYYMMDDを取得（日まで）
     getToday() {
-        const todayData = new Date();
-        const year = todayData.getFullYear();
-        const month = todayData.getMonth() + 1;
-        const date = todayData.getDate();
-        const today = year * 10000 + month * 100 + date;
-        return today;
+      const todayData = new Date();
+      const year = todayData.getFullYear();
+      const month = todayData.getMonth() + 1;
+      const date = todayData.getDate();
+      const today = year * 10000 + month * 100 + date;
+      return today;
     },
     dataCheck(num) {
       //0はタスク名用、1は期限用
@@ -437,45 +477,48 @@ export default {
     },
     // 実績のカウント(mode 0:ページ表示時のログイン日数、1:タスク完了時、2:タスク削除時)
     async countAchievement(mode) {
-      console.log("ログインカウント確認しました");  ///////////////////////////
       const db = getFirestore(this.$app);
       const docSnap = await getDoc(doc(db, "user", this.uid));
-      const today = this.getToday();
       let ad;
       if (docSnap.exists()) ad = docSnap.data();
-      if (mode == 0) {
-        if (ad.last_login != today) {
-          ad.daily_login++;
+      switch (mode) {
+        case 0:
+          if (ad.last_login != today) {
+            const today = this.getToday();
+            ad.daily_login++;
+            await updateDoc(doc(db, "user", this.uid), {
+              last_login: today,
+              daily_login: ad.daily_login,
+            });
+          }
+          break;
+        case 1:
+          const timeDiff = this.getNow() - this.time_created;  // 作成してすぐに完了していないか確認（不正してないか）
+          if (timeDiff < 300) ad.completed_quick++;
+          ad.completed_all++;  // タスク完了の合計カウントを増やす
+          if (this.priority == "高") ad.completed_high++;  // 難易度ごとのカウントを増やす
+          else if (this.priority == "中") ad.completed_middle++;
+          else if (this.priority == "低") ad.completed_low++;
+          const deadlineChecked = Number(this.deadline.replace(/-/g, ''));  // 期限を過ぎていないか確認
+          const today = this.getToday();
+          if (deadlineChecked >= today || deadlineChecked == "") ad.task_success++;
+          else ad.task_failure++;
           await updateDoc(doc(db, "user", this.uid), {
-            last_login: today,
-            daily_login: ad.daily_login,
+            completed_all: ad.completed_all,
+            completed_high: ad.completed_high,
+            completed_low: ad.completed_low,
+            completed_middle: ad.completed_middle,
+            completed_quick: ad.completed_quick,
+            task_failure: ad.task_failure,
+            task_success: ad.task_success,
           });
-        }
-      } else if (mode == 1) {
-        const timeDiff = this.getNow() - this.time_created;  // 作成してすぐに完了していないか確認（不正してないか）
-        if (timeDiff < 300) ad.completed_quick++;          //////////////////// 不正していたら他のカウントを増やすか決める /////////////////
-        ad.completed_all++;  // タスク完了の合計カウントを増やす
-        if (this.priority == "高") ad.completed_high++;  // 難易度ごとのカウントを増やす
-        else if (this.priority == "中") ad.completed_middle++;
-        else if (this.priority == "低") ad.completed_low++;
-        const deadlineChecked = Number(this.deadline.replace(/-/g, ''));  // 期限を過ぎていないか確認
-        const today = this.getToday();
-        if (deadlineChecked >= today || deadlineChecked == "") ad.task_success++;
-        else ad.task_failure++;
-        await updateDoc(doc(db, "user", this.uid), {
-          completed_all: ad.completed_all,
-          completed_high: ad.completed_high,
-          completed_low: ad.completed_low,
-          completed_middle: ad.completed_middle,
-          completed_quick: ad.completed_quick,
-          task_failure: ad.task_failure,
-          task_success: ad.task_success,
-        });
-      } else if (mode == 2) {
-        ad.task_delete++;
-        await updateDoc(doc(db, "user", this.uid), {
-          task_delete: ad.task_delete,
-        });
+          break;
+        case 2:
+          ad.task_delete++;
+          await updateDoc(doc(db, "user", this.uid), {
+            task_delete: ad.task_delete,
+          });
+          break;
       }
     },
     // 履歴ポップアップを開く
@@ -562,6 +605,10 @@ export default {
 </script>
 
 <style>
+:focus {
+  outline: none;
+}
+
 #complete {
   height: 2em;
 }
@@ -590,14 +637,36 @@ export default {
   font-weight: bold;
 }
 
+.button-history {
+  margin-left: 30px;
+  padding: 0 20px;
+  height: 2.5em;
+  border-radius: 3em;
+  background-color: #ffdd44;
+  border: none;
+  box-shadow: 1px 2px 1px #777777;
+}
+
 .button-add {
   float: right;
-  width: 6em;
+  margin-left: 20px;
+  padding: 0 20px;
   height: 2.5em;
   border-radius: 3em;
   background-color: greenyellow;
   color: black;
   border: none;
+  box-shadow: 1px 2px 1px #777777;
+}
+
+.sort_select {
+  float: right;
+  margin: 3px 5px 0;
+  padding: 2px 10px;
+  border-radius: 7px;
+  border: 3px solid #ffdd44;
+  color: black;
+  box-shadow: 1px 1px 2px #777777;
 }
 
 .task {
